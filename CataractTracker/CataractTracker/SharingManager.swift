e0232d36ab733979347a066c7ed6242032a28385
Yongzheng Huang <hahooy1@yongzhengs-air.dhcp.wustl.edu>
2016-11-05 13:01:58 -0500

    more update

--- CataractTracker/CataractTracker/SharingManager.swift
+++ CataractTracker/CataractTracker/SharingManager.swift
@@ -41,8 +41,8 @@ class SharingManager {
         static let NumberOfMomentsToFetch: UInt = 10
         static let minimumTimeInterval = 0.000001
         static let maxThumbnailSize: CGFloat = 1000
-        static let baseServerURL = "http://localhost/archhack/siphon2/"
-        //static let baseServerURL = "http://ec2-54-165-251-2.compute-1.amazonaws.com/siphon2/"
+        //static let baseServerURL = "http://localhost/archhack/siphon2/"
+        static let baseServerURL = "http://ec2-54-165-251-2.compute-1.amazonaws.com/siphon2/"
         static let loginURL = baseServerURL + "login.php/"
         static let searchUserURL = baseServerURL + "search-user/"
         static let uploadMomentURL = baseServerURL + "upload.php/"
